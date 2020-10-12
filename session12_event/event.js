9c14d5273db8c816d9725bfba3e6142f295e9aae
Hung Nguyen <nguyenxuan.hung96@gmail.com>
2020-10-12 21:01:59 +0700

    ss 12 - event

---
+++ session12_event/event.js
@@ -0,0 +1,36 @@
+
+// function helloWorld() {
+//     console.log('123456');
+//     alert('Hello world');
+// }
+
+// let h2Dom = document.getElementById('title');
+// h2Dom.addEventListener('click', helloWorld);
+
+// function welcome(e, t) {
+//     console.log(e);
+//     console.dir(t);
+//     let inputName = document.getElementById('inputName');
+//     let inputPass = document.getElementById('inputPass');
+//     // console.dir(inputName.value);
+//     // alert('Xin chào: ' + inputName.value);
+//     if (inputName.value === 'admin'
+//         && inputPass.value === '123456') {
+//         alert('Thành công');
+//         //window.location = "https://dantri.com.vn";
+//         window.location.href = "https://dantri.com.vn";
+//         //window.location.replace('https://dantri.com.vn');
+//     } else {
+//         alert('Sai tên hoặc pass');
+//     }
+// }
+
+// function enterLogin(e, t){
+//     console.log(e);
+//     console.log(t);
+//     if(e.key === 'Enter'){
+//         welcome();
+//     }
+// }
+
+
